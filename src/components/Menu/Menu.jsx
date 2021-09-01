6a26161441050c7f3825402020e5164f387b3f53
AROMA GUPTA <aromagupta1200@gmail.com>
2021-09-01 22:03:51 +0530

    initial

---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,30 @@
+import "./Menu.scss";
+
+export default function Menu({ menuOpen, setMenuOpen}){
+    return (
+        <div className={"menu " + (menuOpen && "active")}>
+            <ul>
+                <li onClick={() =>setMenuOpen(false)}>
+                     <a href="#intro">HOME</a>
+                </li>
+                <li onClick={() =>setMenuOpen(false)}>
+                     <a href="#edu">Education</a>
+                </li >
+                <li onClick={() =>setMenuOpen(false)}>
+              
+           
+                <li onClick={() =>setMenuOpen(false)}>
+                     <a href="#skills">skills</a>
+                </li>
+                <a href="#projects">projects</a>
+                </li >
+
+                <li onClick={() =>setMenuOpen(false)}>
+                     <a href="#contact">Connect</a>
+                </li>
+            </ul>
+            
+        </div>
+    );
+}
+
